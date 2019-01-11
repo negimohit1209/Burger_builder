7d8aa7c01d3d50bb3f5b37346fa0e33792362599
negimohit1209 <mohitsngh1997@gmail.com>
2019-01-11 14:46:48 +0530

    add navigation

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import classes from "./NavigationItems.module.css";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const NavigationItems = props => {
+  return (
+    <ul className={classes.NavigationItems}>
+      <NavigationItem link="/" active>Burger Builder</NavigationItem>
+      <NavigationItem link="/">Setting</NavigationItem>
+    </ul>
+  );
+};
+
+export default NavigationItems;
