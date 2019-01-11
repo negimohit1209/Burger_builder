daeb3cc7a56b243585262542d01a4d58ccdbc902
negimohit1209 <mohitsngh1997@gmail.com>
2019-01-11 14:18:01 +0530

    add logo in the toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Auxiliary from "../../hoc/Auxiliary";
 import styles from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = props => {
     return (
         <Auxiliary>
+        <Toolbar />
             <div>Toolbar , SideDrawer, BackDrop</div>
             <main className={styles.Content}>{props.children}</main>
         </Auxiliary>
