0f02d16e2d2a3464f914f3da78e881c4ab579996
Mohit Negi <mohitsngh1997@gmail.com>
2019-02-22 01:04:57 +0530

    add router

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,13 +8,18 @@ const withErrorHandler = (WrappedComponent, axios ) => {
           error: null
       }
       componentDidMount() {
-          axios.interceptors.request.use(req => {
+          this.reqInterceptor = axios.interceptors.request.use(req => {
               this.setState({error: null})
           })
-          axios.interceptors.response.use(res => res, error => {
+          this.resInterceptor = axios.interceptors.response.use(res => res, error => {
               this.setState({error: error});
               return Promise.reject(error);
           })
+      }
+
+      componentWillUnmount() {
+        axios.interceptors.request.eject(this.reqInterceptor);
+        axios.interceptors.response.eject(this.resInterceptor);
       }
       errorConfirmedHandler = () => {
           this.setState({error: null})
