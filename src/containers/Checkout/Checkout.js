663a400f2fd353892f03ac4ad760539aad854c09
Mohit Negi <mohitsngh1997@gmail.com>
2019-02-23 15:28:51 +0530

    add checkout page

---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import CheckoutSummary from '../../components/order/CheckoutSummary/CheckoutSummary';
+export default class Checkout extends Component {
+    state  = {
+        ingredients:{
+          salad: 1,
+          meat: 1,
+          cheese: 1,
+          bacon: 1
+        }
+    }
+  render() {
+    return (
+      <div>
+        <CheckoutSummary ingredients={this.state.ingredients}/>
+      </div>
+    )
+  }
+}
