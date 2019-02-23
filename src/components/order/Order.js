07c5b32aade4366424b5da08deac4b1a39af80f6
Mohit Negi <mohitsngh1997@gmail.com>
2019-02-23 20:52:44 +0530

    add my order page

---
+++ src/components/order/Order.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import classes from './Order.module.css';
+const Order = (props) => {
+  const ingredients = [];
+  for (let ingredientName in props.ingredients) {
+    ingredients.push({
+      amount: props.ingredients[ingredientName],
+      name: ingredientName
+    })
+  }
+  let ingredientOutput = ingredients.map(ig => {
+    return <span 
+    key={ig.name}
+    style={{
+      textTransform: 'capitalize',
+      display: 'inline-block',
+      margin: '0 8px',
+      border: '1px solid #ccc',
+      padding: '5px'
+    }}
+    >
+      {ig.name} ({ig.amount})
+    </span>;
+  })
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price <strong>{props.price}</strong></p>
+    </div>
+  )
+}
+
+export default Order
